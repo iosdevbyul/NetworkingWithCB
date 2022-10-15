ca6a0d3609a233428683758bc7b429ce4b46e5cd
Jessi <sbjoo@pntbiz.com>
2022-10-16 01:28:35 +0900

    Initial Commit

---
+++ NetworkingWithCB/NetworkingWithCBApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingWithCBApp.swift
+//  NetworkingWithCB
+//
+//  Created by PNT001 on 2022/10/16.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingWithCBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
